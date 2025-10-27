0fdb10dd474dd27a400087c7b30be8d7205f6c04
chris1797 <jaehun6004@gmail.com>
2025-10-28 00:03:43 +0900

    Initial commit

---
+++ src/main/kotlin/com/reactive/core/SpringReactivePracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.reactive.core
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringReactivePracticeApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringReactivePracticeApplication>(*args)
+}
---
+++ src/test/kotlin/com/reactive/core/SpringReactivePracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.reactive.core
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringReactivePracticeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
